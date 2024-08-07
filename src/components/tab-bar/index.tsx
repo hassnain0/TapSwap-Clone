7fd7ef6f36ae54e725e0ec33657af42a2474a194
Claudia Lima <limamdamaso@gmail.com>
2024-08-07 18:59:14 +0100

    icons tabbar

--- src/components/tab-bar/index.tsx
+++ src/components/tab-bar/index.tsx
@@ -1,6 +1,13 @@
 import { ChartNoAxesColumnIncreasing, ListChecks, Pickaxe, Rocket, Users } from "lucide-react";
 import { NavLink, NavLinkProps } from "react-router-dom";
 
+import lot from "@/assets/tab-bar/lot.svg"
+import stats from "@/assets/tab-bar/stats.svg"
+import t from "@/assets/tab-bar/Group 48095552.svg"
+import al from "@/assets/tab-bar/Group 48095525.svg"
+import ref from "@/assets/tab-bar/Group 48095644.svg"
+
+
 interface LinkProps extends NavLinkProps { }
 
 const Link = (props: LinkProps) => {
@@ -8,8 +15,8 @@ const Link = (props: LinkProps) => {
     <NavLink
       className={({ isActive }) =>
         isActive ?
-          "bg-[#2C3649] flex flex-col w-20 h-20  items-center justify-center gap-2 rounded-xl text-[#38B8EA] border border-[#38B8EA] text-sm" :
-          "bg-[#2C3649] flex w-20 h-20 flex-col items-center justify-center gap-2 rounded-xl text-[#7C7C7C] border border-transparent text-sm"
+          "bg-[#2C3649] flex flex-col w-20 h-20  items-center justify-center gap-1 rounded-xl text-[#38B8EA] border border-[#38B8EA] text-sm" :
+          "bg-[#2C3649] flex w-20 h-20 flex-col items-center justify-center gap-1 rounded-xl text-[#7C7C7C] border border-transparent text-sm"
       }
       {...props}
     >
@@ -24,23 +31,23 @@ export function TabBar() {
     <footer className="w-full">
       <nav className="flex items-center justify-center gap-2 text-[#707489] text-xl pb-4 px-2">
         <Link to="/invites">
-          <Users />
+          <img src={ref} alt="Polygon image" className="w-6" />
           <span>Ref</span>
         </Link>
         <Link to="/socials" >
-          <ListChecks />
+          <img src={t} alt="Polygon image" className="w-6" />
           <span>Tasks</span>
         </Link>
         <Link to="/home" >
-          <Pickaxe />
+          <img src={al} alt="Polygon image" className="w-6" />
           <span>Home</span>
         </Link>
         <Link to="/lottery" >
-          <Rocket />
+          <img src={lot} alt="Polygon image" className="w-6" />
           <span>Boost</span>
         </Link>
         <Link to="/static" >
-          <ChartNoAxesColumnIncreasing />
+          <img src={stats} alt="Polygon image" className="w-6" />
           <span>Stats</span>
         </Link>
       </nav>
--- src/pages/shopping-bag/index.tsx
+++ src/pages/shopping-bag/index.tsx
@@ -1,5 +1,7 @@
 import { ChevronLeft } from "lucide-react"
 
+import polygonImage from "@/assets/dollar.svg"
+
 import { TabBar } from "@/components/tab-bar"
 import { Link } from "react-router-dom"
 
@@ -11,14 +13,19 @@ export function ShoppingBag() {
           <Link to="/lottery" className="h-10 w-10 flex justify-center items-center rounded-lg text-white bg-button">
             <ChevronLeft color="white" size={16} />
           </Link>
-          <strong className="text-white flex-1 text-center text-xl mr-4">My Shopping Bag</strong>
+          <strong className="text-white flex-1 text-center text-xl mr-4">Buy lottery ticket</strong>
         </header>
 
         <main className="flex flex-col items-center mt-12">
-          <p className="text-center text-white/60 text-[15px] font-medium">Participate in the squadman lottery </p>
-          <span className="text-center text-white/60 text-xs block max-w-32">Get a reward of 300 000 coins when you pay</span>
+          <p className="text-center text-white/60 text-[15px] font-medium"> Participate in the squadman lottery</p>
+          <span className="text-center text-white/60 text-xs block max-w-32">Get a reward of
+            <div className="flex items-center gap-1 justify-center my-1">
+              <img src={polygonImage} alt="Polygon image" className="w-3 h-3" />
+              <strong className="text-white">300 000</strong>
+            </div>
+            coins when you pay</span>
 
-          <p className="mt-[74px] mb-20 text-white/80 font-medium text-base">Total: <strong className="text-[#38B8EA] text-base">TON 0002.</strong></p>
+          <p className="mt-[74px] mb-20 text-white/80 font-medium text-base">Total: <strong className="text-[#38B8EA] text-base">US $1</strong></p>
           <button className="h-11 rounded-lg bg-button text-white px-4 text-base font-medium min-w-[160px]">
             Pay now
           </button>
